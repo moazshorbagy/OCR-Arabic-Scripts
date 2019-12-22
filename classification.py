3cf1903004734c85adc02572f1cce1c3a9221fd6
muhammad20 <muhammadahmad201196@hotmail.com>
2019-12-22 21:45:59 +0200

    testing model

--- classification.py
+++ classification.py
@@ -1,6 +1,29 @@
 # ===================== #
 # Getting Training Data #
 # ===================== #
+from preprocessing import *
+import numpy as np
+# from keras.utils import np_utils
+# from sklearn.preprocessing import LabelEncoder
+
+# data, labels, errors = get_char_images('verification/scanned', 'verification/text', 0, 1)
+# save_char_imgs(labels, 'chars')
+# save_labels(data)
+# save_features('chars', 0, 5628)
+
+X, Y = load_dataset('features.txt', 'labels.txt')
+
+# def shape_labels(Y, num_classes=29):
+#     labels = []
+#     for i in range(len(Y)):
+#         label = np.zeros(num_classes)
+#         label[Y[i] - 1] += 1
+#         labels.append(label)
+#     labels = np.asarray(labels)
+#     return labels
+# Y = shape_labels(Y)
+# Y = np_utils.to_categorical(Y)
+# print(Y[0,:])
 
 # chars = []
 
@@ -21,7 +44,7 @@ pca = PCA(n_components = 20)
 
 
 from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.35, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.35, random_state = 0)
 
 # =========================== #
 # Building Model and Training #
@@ -44,14 +67,10 @@ clfMLP = MLPRegressor(hidden_layer_sizes=(6, 6))
 
 
 from sklearn.neural_network import MLPClassifier
-X = [[0., 0.], [1., 1.]]
-y = [[0, 1], [1, 1]]
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
-clf.fit(X, y)
-
-X_pred = [
-    [0., 0.],
-    [1., 2.],
-    [1., 1.]
-]
-print(clf.predict(X_pred))
+from sklearn.metrics import accuracy_score
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,15), random_state=1, max_iter=10000)
+clf.fit(X_train, y_train)
+y_predicted = clf.predict(X_test)
+print(y_predicted[0])
+print(y_test[0])
+print(accuracy_score(y_test, y_predicted))
