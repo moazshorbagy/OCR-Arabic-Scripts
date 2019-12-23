b45de3868f3c2a39f29bbe9c92ea1e63c1c9887e
muhammad20 <muhammadahmad201196@hotmail.com>
2019-12-23 14:51:34 +0200

    ..

--- ocr.py
+++ ocr.py
@@ -6,12 +6,14 @@ from utility import vertical_histogram
 import skimage.io as io
 import numpy as np
 from classification import *
+from time import time
 
 if __name__=='__main__':
 
-    flag=True
-    modelPath="zaki.sav"
-    image="verification/scanned/capr7.png"
+    flag=False
+    modelPath="zaki200.sav"
+    image="verification/scanned/capr1.png"
+    was = time()
     if flag:
         model=load_model(modelPath)
         words = get_char_images_pred(image)
@@ -21,7 +23,7 @@ if __name__=='__main__':
                 X_test.append(get_features(char, False))
         
     else:
-        data, labels, errors, words_lengths = get_char_images('verification/scanned', 'verification/text', 0, 20)
+        data, labels, errors, words_lengths = get_char_images('verification/scanned', 'verification/text', 0, 200)
         features = []
         for char in data:
             features.append(get_features(char, False))
@@ -33,7 +35,11 @@ if __name__=='__main__':
         model = build_model(features,labels)
         save_model(model,modelPath)
 
+
     predictions = model.predict(X_test)
+    now = int(time() - was)
+    print(now)
+    print(predictions)
     new_pred = []
     current = 0
     for i in range(len(words)):
@@ -42,4 +48,4 @@ if __name__=='__main__':
             current += 1
         new_pred.append(0)
     # X_test = np.asarray(X_test)
-    save_predictions(new_pred,"test/capr7.txt")
+    save_predictions(new_pred,"test/capr1.txt")
